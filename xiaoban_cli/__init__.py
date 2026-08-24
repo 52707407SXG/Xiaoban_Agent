4b8bdefbe308b3d8a57fd45dfb8a88e8f925cbdb
Yuan Laoshi <yuan-laoshi@zyj028.com>
2026-08-25 01:59:48 +0800

    chore: bump Xiaoban to v0.17.2

--- xiaoban_cli/__init__.py
+++ xiaoban_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.17.1"
-__release_date__ = "2026.7.6"
+__version__ = "0.17.2"
+__release_date__ = "2026.8.25"
 
 
 def _ensure_utf8():
